3674d2f018c4b3f39b35e106ea282cd3e1f8c47c
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2025-07-09 22:29:59 +0530

    Setup prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { v4 as uuid } from "uuid";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
+  main();
 });
+
+async function main() {
+  console.log("Connected to database");
+  try {
+    const newOrganization = await prisma.Organization.create({
+      data: {
+        id: uuid(),
+        name: "Test Organization",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+    console.log(newOrganization);
+  } catch (error) {
+    console.log(error);
+  }
+}
